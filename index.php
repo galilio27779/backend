870ed27edb80d0d9eb4747b36bceb1c7c8887c66
galilio27779 <mohgal27779@gmail.com>
2025-04-12 22:45:48 +0200

    Add files via upload
    task2

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$name="Mohammed Gallaledin";
+echo "     <h3>      WELCOME - <h1><br>Your name is :  <u>";
+echo ($name);
+?>
+<hr>
+
+<?php
+$x=15;
+$z=75;
+$y=$x+$z;
+echo "y=x+z= ". $y;
+?>
+<hr>
+<?php
+$x=10;
+echo "x=".$x ."<br>";
+echo "x++=". $x++ ." <br> ". "++x=".++$x;
+
+?>
+<hr>
+<?php
+$sentence =null;
+$first="In";
+$second="PHP";
+$third="Course";
+echo "The sentence is:__"."$first"."_"."$second"."_" ."$third";
+
+?>
